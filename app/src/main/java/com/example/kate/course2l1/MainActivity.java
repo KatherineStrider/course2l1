98ee79c6aa84ec9b9063459e80e2393ebff88f4c
KatherineStrider <kotopesska112@yandex.ru>
2017-05-13 01:22:50 +0400

    Доработка класса для получания информации из двух таблиц

--- app/src/main/java/com/example/kate/course2l1/MainActivity.java
+++ app/src/main/java/com/example/kate/course2l1/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.kate.course2l1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.example.kate.course2l1.SQLite.DBEmployee;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.view_list);
+
+        String SQLQuery = "SELECT " +
+                DBEmployee.TableEmployee.T_NAME +  "." + DBEmployee.TableEmployee._ID + ", " +
+                DBEmployee.TableEmployee.T_NAME +  "." + DBEmployee.TableEmployee.C_NAME + ", " +
+                DBEmployee.TableEmployee.C_INFO + ", " +
+                DBEmployee.TableDepartment.C_DEP + ", " +
+                DBEmployee.TableDepartment.C_LOCA +
+                " FROM " +
+                DBEmployee.TableEmployee.T_NAME +
+                " INNER JOIN " + DBEmployee.TableDepartment.T_NAME +
+                " ON " + DBEmployee.TableEmployee.T_NAME +  "." + DBEmployee.TableEmployee.C_DEP_ID + " = " +
+                DBEmployee.TableDepartment.T_NAME + "." + DBEmployee.TableDepartment._ID;
+
+        Cursor c = App.getDB().getReadableDatabase().rawQuery(SQLQuery, null);
+
+        String[] from = {DBEmployee.TableEmployee.C_NAME, DBEmployee.TableEmployee.C_INFO, DBEmployee.TableDepartment.C_DEP, DBEmployee.TableDepartment.C_LOCA};
+        int[] to = {R.id.empName, R.id.empInfo, R.id.depName, R.id.depLocation};
+
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                R.layout.list_item, c, from, to, 1);
+        listView.setAdapter(adapter);
+
     }
 }
